cd2647d988f2ff7e756bfe840be767df92d9b9bf
David Espino <davidespinog@gmail.com>
2023-05-02 11:19:38 -0600

    Update queries, schema and data.sql

--- queries.sql
+++ queries.sql
@@ -24,3 +24,67 @@ WHERE name NOT LIKE 'Gabumon';
 
 SELECT * FROM animals
 WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+ROLLBACK;
+
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%___mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+BEGIN; 
+
+DELETE FROM animals; 
+
+ROLLBACK;
+
+-- +++++++++++++++
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT sp1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO sp1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
+
+-- +++++++++++++++
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals
+WHERE escape_attempts > 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT AVG(escape_attempts), neutered FROM animals
+GROUP BY neutered;
+
+SELECT MIN(weight_kg), MAX(weight_kg), species FROM animals
+GROUP BY species;
+
+SELECT AVG(escape_attempts), species FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01'
+GROUP BY species;
